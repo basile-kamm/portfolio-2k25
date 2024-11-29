db3141a289ec833350d5fdd3a0c36e48e2df46c4
basile-kamm <another-black-kitty@proton.me>
2024-11-29 14:11:21 +0100

    ye

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,40 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Banner anim
+  const parallax = gsap.timeline({
+    scrollTrigger: {
+      markers: true,
+      trigger: ".parallax-container",
+      start: "top top",
+      end: "bottom top",
+      pin: ".parallax-container",
+      scrub: 0.3,
+    },
+  });
+
+  parallax.to(".layer-1", {
+    y: -50,
+  });
+  parallax.fromTo(
+    ".layer-1-2",
+    {
+      y: -100,
+    },
+    {
+      y: 100,
+    },
+    "<"
+  );
+
+  parallax.from(
+    ".selected-background",
+    {
+      scale: 30,
+      y: -1000,
+    },
+    "0.6"
+  );
+});
