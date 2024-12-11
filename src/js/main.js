519283fba25149c4c0df3b9535cf2e9961cca27e
basile-kamm <another-black-kitty@proton.me>
2024-12-11 10:07:40 +0100

    working banner anim

--- src/js/main.js
+++ src/js/main.js
@@ -16,25 +16,102 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   parallax.to(".layer-1", {
-    y: -50,
+    y: "-6vw",
+    duration: 1,
   });
   parallax.fromTo(
     ".layer-1-2",
     {
-      y: -100,
+      y: "23vw",
     },
     {
-      y: 100,
+      y: "18vw",
+      duration: 1,
     },
     "<"
   );
-
+  parallax.to(
+    ".layer-2",
+    {
+      y: "-4vw",
+      duration: 1,
+    },
+    "<"
+  );
+  parallax.to(
+    ".banner-titles",
+    {
+      y: -30,
+      duration: 1,
+    },
+    "<"
+  );
+  parallax.to(
+    ".banner-texts",
+    {
+      y: -40,
+      duration: 1,
+    },
+    "<"
+  );
+  parallax.to(
+    ".layer-3",
+    {
+      y: -20,
+      duration: 1,
+    },
+    "<"
+  );
+  parallax.to(
+    ".layer-4",
+    {
+      y: 20,
+      duration: 1,
+    },
+    "<"
+  );
+  parallax.to(
+    ".layer-5",
+    {
+      y: 50,
+      duration: 1,
+    },
+    "<"
+  );
+  parallax.to(
+    ".layer-background",
+    {
+      y: 70,
+      duration: 1,
+    },
+    "<"
+  );
   parallax.from(
     ".selected-background",
     {
       scale: 30,
-      y: -1000,
+      y: "-170vw",
+      rotate: -45,
+
+      duration: 1,
     },
-    "0.6"
+    "<"
+  );
+  parallax.to(
+    ".banner-container",
+    {
+      scale: 0.35,
+      duration: 0.3,
+      y: 100,
+    },
+    "<0.75"
+  );
+  parallax.from(
+    ".selected-works",
+    {
+      translateY: "200%",
+      duration: 0.5,
+    },
+    "<"
   );
 });
