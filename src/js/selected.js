b7191f2084006fafcd9680179880d558b63c5687
basile-kamm <another-black-kitty@proton.me>
2024-12-23 20:02:46 +0100

    jsp trop là

--- src/js/selected.js
+++ src/js/selected.js
@@ -4,14 +4,18 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 document.addEventListener("DOMContentLoaded", function () {
   const selectedWorks = document.querySelectorAll(".selected-work");
   const selectedTrailers = document.querySelectorAll(".selected-trailer");
-  console.log("Selected works:", selectedWorks);
+  const selectedDetails = document.querySelectorAll(
+    ".selected-detail-container"
+  );
+  const selectedCloseMap = document.querySelector(".selected-detail-close-map");
+
+  const body = document.querySelector("body");
 
   selectedWorks.forEach((selectedWork) => {
     let hoverAnim = null; // Initialize variable to store animation instance
     const hoverValue = selectedWork.getAttribute("data-selected-value");
 
     selectedWork.addEventListener("mouseenter", function () {
-      console.log(hoverValue);
       hoverAnim = selectedWorkHover(selectedWork);
 
       selectedTrailers.forEach((trailer) => {
@@ -36,7 +40,60 @@ document.addEventListener("DOMContentLoaded", function () {
         });
       }
     });
+    selectedWork.addEventListener("click", function () {
+      selectedDetails.forEach((selectedWorkDetail) => {
+        if (selectedWorkDetail.classList.contains(hoverValue)) {
+          const selectedDetailClose = selectedWorkDetail.querySelector(
+            ".selected-detail-close"
+          );
+          const openDetailAnim = openWorkDetail(
+            selectedWorkDetail,
+            selectedDetailClose
+          );
+          selectedDetailClose.onclick = function () {
+            openDetailAnim.reverse();
+          };
+          selectedCloseMap.addEventListener("click", function () {
+            openDetailAnim.reverse();
+          });
+        }
+      });
+    });
   });
+
+  function openWorkDetail(selectedWorkDetail, selectedDetailClose) {
+    const tl = gsap.timeline({
+      onReverseComplete: () => {
+        tl.set(selectedWorkDetail, { display: "none" });
+        body.classList.remove("no-scroll");
+      },
+    });
+    body.classList.add("no-scroll");
+    tl.set(selectedWorkDetail, { display: "block" });
+    tl.set(selectedDetailClose, { zIndex: 10 });
+    tl.set(selectedCloseMap, { display: "block" });
+    tl.to(selectedWorkDetail, { translateX: 0 });
+    tl.fromTo(
+      selectedWorkDetail.querySelectorAll(".open-anim"),
+      {
+        x: 400,
+      },
+      {
+        x: 0,
+        stagger: 0.1,
+      },
+      "<0.2"
+    );
+    tl.to(
+      selectedCloseMap,
+      {
+        opacity: 1,
+      },
+      "<"
+    );
+
+    return tl;
+  }
 
   function selectedWorkHover(selectedWork) {
     const tl = gsap.timeline();
